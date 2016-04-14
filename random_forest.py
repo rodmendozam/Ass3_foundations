ccee9a303cfd9540099a2074c19d4a2ab0352b13
Rodrigo Mendoza <rmendoza@mendozablanco.com.mx>
2016-04-14 11:12:08 +0200

    no message

--- random_forest.py
+++ random_forest.py
@@ -9,6 +9,8 @@ from sklearn.metrics import zero_one_loss, roc_auc_score
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import KFold, Bootstrap, ShuffleSplit
 from sklearn import cross_validation
+from sklearn.tree import DecisionTreeClassifier
+
 
 # #connect to openml api
 # apikey = 'ca2397ea8a2cdd9707ef39d76576e786'
@@ -68,7 +70,7 @@ def exercise_2():
         error.append( np.array(error_mean).mean() )
     #plot
     plt.style.use('ggplot')
-    plt.scatter(lst, error)
+    plt.plot(lst, error, '#009999', marker='o')
     plt.xticks(lst)
     plt.show()
 
@@ -79,23 +81,32 @@ def exercise_3():
     dataset = connector.download_dataset(44)
     X, y, attribute_names = dataset.get_dataset(target=dataset.default_target_attribute, return_attribute_names=True)
 
-    kf = cross_validation.ShuffleSplit(len(X),n_iter=100, test_size=0.1, train_size=0.9, random_state=0)
+
+    kf = cross_validation.ShuffleSplit(len(X),n_iter=10, test_size=0.1, train_size=0.9, random_state=0)
     error = []
+    error_cart = []
     error_mean = []
+    error_mean_cart = []
 
     clf = RandomForestClassifier(n_estimators=100, oob_score=True,
                                    max_features="auto",
                                    random_state=0)
-
+    clf_cart = DecisionTreeClassifier()
     error_mean = []
     for train_index, test_index in kf:
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         clf.fit(X_train, y_train)
-        error_mean.append( roc_auc_score(y_test, clf.predict(X_test)) )
-    error.append( np.array(error_mean).mean() )
+        clf_cart.fit(X_train, y_train)
 
-    print error
+        error_mean.append( roc_auc_score(y_test, clf.predict(X_test)) )
+        error_mean_cart.append( roc_auc_score(y_test, clf_cart.predict(X_test)) )
+
+    error.append( np.array(error_mean).mean() )
+    error_cart.append( np.array(error_mean_cart).mean() )
+
+    print 'Error RandomForest: ', error
+    print 'Error CART: ', error_cart
 
     #plot
     # plt.style.use('ggplot')
@@ -103,6 +114,88 @@ def exercise_3():
     # plt.xticks(lst)
     # plt.show()
 
+def bias_exercise3():
+    #connect to openml api
+    apikey = 'ca2397ea8a2cdd9707ef39d76576e786'
+    connector = APIConnector(apikey=apikey)
+    dataset = connector.download_dataset(44)
+    X, y, attribute_names = dataset.get_dataset(target=dataset.default_target_attribute, return_attribute_names=True)
+
+
+    kf = cross_validation.ShuffleSplit(len(X),n_iter=10, test_size=0.1, train_size=0.9, random_state=0)
+    error = []
+    error_mean = []
+    lst = [int(math.pow(2, i)) for i in range(0, 8)]
+
+    clf = RandomForestClassifier(oob_score=True,
+                                   max_features="auto",
+                                   random_state=0)
+    for i in lst:
+        error_mean = []
+        clf.set_params(n_estimators=i)
+        for train_index, test_index in kf:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            clf.fit(X_train, y_train)
+            predicted_elements = clf.predict(X_test)
+
+            for i in range(0, len(y_test)):
+                error_mean.append( (y_test[i] - predicted_elements[i])*(y_test[i] - predicted_elements[i])  )
+        error.append( np.array(error_mean).mean() )
+
+    plt.style.use('ggplot')
+    plt.plot(lst, error, '#009999', marker='o')
+    plt.xticks(lst)
+    plt.margins(0.02)
+    plt.xlabel('number of trees')
+    plt.ylabel('Bias Squared')
+    plt.show()
+
+def variance_exercise3():
+    apikey = 'ca2397ea8a2cdd9707ef39d76576e786'
+    connector = APIConnector(apikey=apikey)
+    dataset = connector.download_dataset(44)
+    X, y, attribute_names = dataset.get_dataset(target=dataset.default_target_attribute, return_attribute_names=True)
+
+
+    kf = cross_validation.ShuffleSplit(len(X),n_iter=10, test_size=0.1, train_size=0.9, random_state=0)
+    total_variance = []
+    variance_fold = []
+    lst = [int(math.pow(2, i)) for i in range(0, 8)]
+
+    clf = RandomForestClassifier(oob_score=True,
+                                   max_features="auto",
+                                   random_state=0)
+
+
+    for i in lst:
+        variance_fold = []
+        clf.set_params(n_estimators=i)
+        for train_index, test_index in kf:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            clf.fit(X_train, y_train)
+
+
+            predicted_elements = clf.predict(X_test)
+
+            # for i in range(0, len(y_test)):
+            variance_fold.append( predicted_elements )
+        total_variance.append( np.array(variance_fold).var() )
+
+    plt.style.use('ggplot')
+    plt.plot(lst, total_variance, '#009999', marker='o')
+    plt.xticks(lst)
+    plt.margins(0.02)
+    plt.xlabel('number of trees')
+    plt.ylabel('Variance')
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
-    exercise_3()
+    # exercise_3()
+    variance_exercise3()
+    # bias_exercise3()
